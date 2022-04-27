4d3ff7b4f83bb4119d2e3ec9ccef76a671b2b141
cchem <chem.camellia@gmail.com>
2022-04-27 22:23:21 +0900

    Storageに関するロジックをまとめた。

--- src/vending_machine/storage.py
+++ src/vending_machine/storage.py
@@ -1,11 +1,33 @@
 from typing import Sequence
 
 from src.drink.drink import Drink
+from src.money.money import Money
 
 
 class Storage:
-    def __init__(self, drink_lineup: Sequence[Drink], number: int):
-        self.storage = {key: val for key, val in drink_lineup, number}
+    def __init__(self, kinds: Sequence, numbers: Sequence[int]):
+        self.storage = {key: val for key, val in kinds, numbers}
 
-    def has_stock(self, drink: Drink):
-        return self.storage[drink] > 0
+    def exists(self, kind):
+        return self.storage[kind] > 0
+
+    def get(self, drink: Drink):
+        if self.exists(drink):
+            self.storage -= 1
+            return drink
+        else:
+            return None
+
+
+class DrinkStorage(Storage):
+    def __init__(self, kinds: Sequence[Drink], numbers: Sequence[int]):
+        if not all([isinstance(i, Drink) for i in kinds]):
+            raise RuntimeError('Key must be object of Drink')
+        super().__init__(kinds, numbers)
+
+
+class MoneyStorage(Storage):
+    def __init__(self, kinds: Sequence[Money], numbers: Sequence[int]):
+        if not all([isinstance(i, Money) for i in kinds]):
+            raise RuntimeError('Key must be object of Drink')
+        super().__init__(kinds, numbers)
